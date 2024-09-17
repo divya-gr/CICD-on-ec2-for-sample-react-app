6fb6089ed208770621db68f7df23d94f0030d913
Divya <111332116+divya-gr@users.noreply.github.com>
2024-09-17 13:07:34 +0530

    main.tf

--- EC2/main.tf
+++ EC2/main.tf
@@ -6,7 +6,75 @@ resource "aws_instance" "main" {
 
   subnet_id       = var.subnet_id
   security_groups = [var.security_group_id]
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update the server
+              sudo apt update -y
+              
+              sudo apt install -y nginx nodejs npm
+
+              # Clone the React app repository
+              sudo mkdir -p /var/www/html
+              sudo git clone https://gitlab.com/decrypt-development/rev-token.git /var/www/html/rev-token
+
+              # Navigate to the app directory
+              cd /var/www/html/rev-token
+
+              # Install dependencies and build the app
+              sudo npm install --legacy-peer-deps
+              sudo npm run build
+
+              # Configure Nginx
+              sudo tee /etc/nginx/sites-available/rev-token.conf <<EOF
+              server {
+                  server_name rev-token.blockchainaustralia.link;
+                  root /var/www/html/rev-token/build;
+                  index index.html;
+                  client_max_body_size 900M;
+
+                  location / {
+                      try_files \$uri \$uri/ /index.html?\$args;
+                  }
+
+                  location /api {
+                      proxy_pass https://0.0.0.0:3000;
+                      proxy_http_version 1.1;
+                      proxy_set_header Upgrade \$http_upgrade;
+                      proxy_set_header Connection 'upgrade';
+                      proxy_set_header Host \$host;
+                      proxy_cache_bypass \$http_upgrade;
+                      proxy_set_header X-Real-IP \$remote_addr;
+                      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                      proxy_read_timeout 1000;
+                      proxy_connect_timeout 1000;
+                      proxy_send_timeout 1000;
+                      proxy_buffers 4 256k;
+                      proxy_buffer_size 128k;
+                      proxy_busy_buffers_size 256k;
+                  }
+
+                  location ~* \.(jpg|jpeg|png|gif|ico|css|js)$ {
+                      expires 1d;
+                  }
+              }
+              EOF
+
+              # Enable the new Nginx configuration
+              sudo ln -s /etc/nginx/sites-available/rev-token.conf /etc/nginx/sites-enabled
+              sudo nginx -t
+              sudo systemctl reload nginx
+
+              # Install Certbot and obtain SSL certificate
+              sudo apt install -y certbot python3-certbot-nginx
+              sudo certbot --nginx --non-interactive --agree-tos --email divya@example.com
+
+              # Clean up
+              sudo rm -rf /var/www/html/rev-token/package-lock.json
+
+              EOF
   tags = {
-    Name = "main-ec2"
+    Name = "web-server"
   }
+
+  
 }
